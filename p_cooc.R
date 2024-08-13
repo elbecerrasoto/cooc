78bc6e968c2d6420ea990f561d70df41a85c6158
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-08-13 09:29:30 -0700

    EDA report

--- p_cooc.R
+++ p_cooc.R
@@ -58,3 +58,74 @@ print(glue("Excluded: {nrow(pairs_bad)} from meta"))
 
 pairs <- pairs |>
   filter(ordered_DE(strand_1, strand_2, query_1, query_2))
+
+
+
+# EDA ---------------------------------------------------------------------
+
+plot_missing(meta)
+plot_missing(pairs)
+
+L_LEVEL <- c(
+  "Complete Genome",
+  "Chromosome",
+  "Scaffold",
+  "Contig"
+)
+
+L_STRAND <- c(
+  "+",
+  "-"
+)
+
+L_QUERY <- c(
+  "YwqJ-deaminase",
+  "Endonuclease_5"
+)
+
+
+meta <- meta |>
+  select(-status) |>
+  mutate(
+    tax_id = as_factor(tax_id),
+    level = factor(level,
+      levels = L_LEVEL,
+      ordered = TRUE
+    ),
+    cds = as.integer(cds)
+  )
+
+
+pairs <- pairs |>
+  mutate(
+    distance = as.integer(distance),
+    genes_inbet = as.integer(genes_inbet),
+    order_1 = as.integer(order_1),
+    order_2 = as.integer(order_2),
+    start_1 = as.integer(start_1),
+    start_2 = as.integer(start_2),
+    end_1 = as.integer(end_1),
+    end_2 = as.integer(end_2),
+    strand_1 = factor(strand_1,
+      levels = L_STRAND
+    ),
+    strand_2 = factor(strand_2,
+      levels = L_STRAND
+    ),
+    query_1 = factor(query_1,
+      levels = L_QUERY
+    ),
+    query_2 = factor(query_2,
+      levels = L_QUERY
+    )
+  )
+
+create_report(meta,
+  output_file = "meta.html",
+  report_title = "Genome Metadata Bacillota Report"
+)
+
+create_report(pairs,
+  output_file = "pairs.html",
+  report_title = "Deaminase Endonuclease Bacillota Pairs Report"
+)
